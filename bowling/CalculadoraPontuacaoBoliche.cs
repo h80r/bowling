0a59a9da71b68bd9e266472dae5cc06a8258ac25
h80r <h80rgbc@gmail.com>
2020-02-06 15:36:50 -0300

    0.2 - Nomes de variáveis melhorados, divisão das classes, e melhor organização da classe Calculadora.

---
+++ bowling/CalculadoraPontuacaoBoliche.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bowling
+{
+	class CalculadoraPontuacaoBoliche
+	{
+		public static int pontuacaoDoJogo(int[] jogadas)
+		{
+			int pontuacaoFinal = 0;
+
+			for (int jogadaAtual = 0, rodadaAtual = 0, pontuacaoRodada; rodadaAtual < 10; rodadaAtual++, jogadaAtual++)
+			{
+				pontuacaoRodada = jogadas[jogadaAtual];
+
+				if (pontuacaoRodada == 10) //strike
+				{
+					pontuacaoRodada += jogadas[jogadaAtual + 1] + jogadas[jogadaAtual + 2];
+				}
+				else //spare & normal
+				{
+					jogadaAtual++;
+					pontuacaoRodada += jogadas[jogadaAtual];
+					if (pontuacaoRodada == 10) //spare
+					{
+						pontuacaoRodada += jogadas[jogadaAtual + 1];
+					}
+				}
+
+				pontuacaoFinal += pontuacaoRodada;
+			}
+			return pontuacaoFinal;
+		}
+	}
+}
--- bowling/Program.cs
+++ bowling/Program.cs
@@ -10,43 +10,12 @@ namespace bowling
 				//Retorno esperado = 133
 			//int[] pinosDerrubados = { 0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  10,  2, 3,  0, 0 };
 				//Retorno esperado = 20
-			//int[] pinosDerrubados = { 0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  2, 8,  2, 3,  0, 0 };
+			int[] pinosDerrubados = { 0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  2, 8,  2, 3,  0, 0 };
 				//Retorno esperado = 17
-			int[] pinosDerrubados = { 10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10 };
-				//Retorno esperado = 300
+			//int[] pinosDerrubados = { 10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10 };
+			//Retorno esperado = 300
 
 			Console.WriteLine(CalculadoraPontuacaoBoliche.pontuacaoDoJogo(pinosDerrubados));
-		}
-	}
-
-	class CalculadoraPontuacaoBoliche
-	{
-		public static int pontuacaoDoJogo (int[] jogadas)
-		{
-			int result = 0;
-
-			for (int load = 0, round = 0, temp; round < 10; round++, load++)
-			{
-				temp = jogadas[load];
-				if (temp == 10) //strike
-				{
-					result += temp + jogadas[load + 1] + jogadas[load + 2];
-				}
-				else
-				{
-					load++;
-					temp += jogadas[load];
-					if (temp == 10) //spare
-					{
-						result += temp + jogadas[load + 1];
-					}
-					else //normal
-					{
-						result += temp;
-					}
-				}
-			}
-			return result;
 		}
 	}
 }
