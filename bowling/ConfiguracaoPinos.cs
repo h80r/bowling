f82e02ce6f49f80bf91257aa2861984ef7056015
h80r <h80rgbc@gmail.com>
2020-02-06 16:05:01 -0300

    0.5 - MENU ADDED - Possíbilidade de escolhas feitas pelo usuário entre padrões predeterminados. - Padrões movidos para classe "ConfiguracaoPinos"

---
+++ bowling/ConfiguracaoPinos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bowling
+{
+	class ConfiguracaoPinos
+	{
+		public static int[] completoSpareStrike = { 1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6 };
+		//Retorno esperado = 133
+
+		public static int[] simplesStrike = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 2, 3, 0, 0 };
+		//Retorno esperado = 20
+
+		public static int[] simplesSpare = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 8, 2, 3, 0, 0 };
+		//Retorno esperado = 17
+
+		public static int[] completoPerfeito = { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+		//Retorno esperado = 300
+	}
+}
--- bowling/Program.cs
+++ bowling/Program.cs
@@ -6,14 +6,35 @@ namespace bowling
 	{
 		static void Main(string[] args)
 		{
-			//int[] pinosDerrubados = { 1, 4,  4, 5,  6, 4,  5, 5,  10,  0, 1,  7, 3,  6, 4,  10,  2, 8, 6 };
-				//Retorno esperado = 133
-			//int[] pinosDerrubados = { 0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  10,  2, 3,  0, 0 };
-				//Retorno esperado = 20
-			int[] pinosDerrubados = { 0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  0, 0,  2, 8,  2, 3,  0, 0 };
-				//Retorno esperado = 17
-			//int[] pinosDerrubados = { 10,  10,  10,  10,  10,  10,  10,  10,  10,  10, 10, 10 };
-			//Retorno esperado = 300
+			int opcaoPinos;
+			int[] pinosDerrubados;
+
+			Console.WriteLine("Que configuração você deseja testar?");
+			Console.WriteLine("1 - Jogo Completo com Spare e Strike");
+			Console.WriteLine("2 - Jogo Simples com Strike");
+			Console.WriteLine("3 - Jogo Simples com Spare");
+			Console.WriteLine("4 - Jogo Completo Perfeito");
+			opcaoPinos = int.Parse(Console.ReadLine());
+
+			switch (opcaoPinos)
+			{
+				case 1:
+					pinosDerrubados = ConfiguracaoPinos.completoSpareStrike;
+					break;
+				case 2:
+					pinosDerrubados = ConfiguracaoPinos.simplesStrike;
+					break;
+				case 3:
+					pinosDerrubados = ConfiguracaoPinos.simplesSpare;
+					break;
+				case 4:
+					pinosDerrubados = ConfiguracaoPinos.completoPerfeito;
+					break;
+				default:
+					Console.WriteLine("Opção inserida inválida.\nO teste será efetuado com a opção 1.");
+					pinosDerrubados = ConfiguracaoPinos.completoSpareStrike;
+					break;
+			}
 
 			Console.WriteLine(CalculadoraPontuacaoBoliche.pontuacaoDoJogo(pinosDerrubados));
 		}
